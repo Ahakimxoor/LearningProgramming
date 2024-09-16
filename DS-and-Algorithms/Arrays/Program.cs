a1e96564ac48051698edd418219e09bc8a07aca9
abdihakim mohamed <abdihakimmohamad090@outlook.com>
2024-09-16 14:08:48 +0300

    bubble sort

--- DS-and-Algorithms/Arrays/Program.cs
+++ DS-and-Algorithms/Arrays/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+public class Program 
+{
+    public static void Main(string[] args)
+    {
+        int[] num = {43, 34, 12, 65, 6, 78};
+        int n = num.Length - 1;
+
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < n - i; j++)
+            {
+                if(num[j] > num[j + 1])
+                {
+                    int tmp = num[j + 1];
+                    num[j + 1] = num[j];
+                    num[j] = tmp;
+                }
+               
+            }
+        }
+
+        for (int i = 0; i < num.Length; i++)
+        {
+              Console.Write(num[i] + " ");
+        }
+
+    }
+}
--- DS-and-Algorithms/Arrays/obj/Debug/net8.0/Arrays.AssemblyInfo.cs
+++ DS-and-Algorithms/Arrays/obj/Debug/net8.0/Arrays.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Arrays")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0e6f8e3f3ef8c19cd416330d1bdca547be5a7d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70021a99b207eef30af683be0a6c9d19a0f24536")]
 [assembly: System.Reflection.AssemblyProductAttribute("Arrays")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Arrays")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
